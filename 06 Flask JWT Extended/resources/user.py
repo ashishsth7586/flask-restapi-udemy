4b0797dc9532d69822df7c9ed6561043f696c72f
Ashish Shrestha <ashishsth7586@gmail.com>
2020-05-20 00:34:27 +0545

    Section06: Flask jwt extended package implemented with Blackist, logout features

---
+++ 06 Flask JWT Extended/resources/user.py
@@ -0,0 +1,85 @@
+import sqlite3
+from flask_restful import Resource, reqparse
+from models.user import UserModel
+from werkzeug.security import safe_str_cmp
+from flask_jwt_extended import (
+    create_access_token, 
+    create_refresh_token, 
+    jwt_refresh_token_required,
+    get_jwt_identity,
+    jwt_required,
+    get_raw_jwt
+)
+from blacklist import BLACKLIST
+
+
+_user_parser = reqparse.RequestParser()
+_user_parser.add_argument('username', type=str, required=True, help="Username cannot be left blank.")
+_user_parser.add_argument('password', type=str, required=True, help="Password cannot be left blank.")
+
+class UserRegister(Resource):
+
+    def post(self):
+
+        data = _user_parser.parse_args()
+
+        if UserModel.find_by_username(data['username']):
+            return {"message": "User with {} already exists".format(data['username'])}, 400
+
+        user= UserModel(**data)
+        user.save_to_db()
+
+        return {"message": "User Created successfully."}, 201
+
+
+class User(Resource):
+    @classmethod
+    def get(cls, user_id):
+        user = UserModel.find_by_id(user_id)
+        if not user:
+            return {'message': 'User not found'}, 404
+        return user.json()
+    
+    @classmethod
+    def delete(cls, user_id):
+        user = UserModel.find_by_id(user_id)
+        if not user:
+            return {'message': 'User not found'}, 404
+        user.delete_from_db()
+        return {'message': 'User deleted!'}, 200
+
+
+class UserLogin(Resource):
+    
+    @classmethod
+    def post(cls):
+        # get data from parser
+        data = _user_parser.parse_args() # returns a dict
+
+        user = UserModel.find_by_username(data['username'])
+
+        if user and safe_str_cmp(user.password, data['password']):
+            access_token = create_access_token(identity=user.id, fresh=True)
+            refresh_token = create_refresh_token(user.id)
+            return {
+                'access_token': access_token,
+                'refresh_token': refresh_token
+            }, 200
+        return {'message': 'Invalid Credentials'}, 401
+
+
+class UserLogout(Resource):
+    @jwt_required
+    def post(self):
+        jti = get_raw_jwt()['jti'] # jwt id, a unique identifier for a JWT
+        BLACKLIST.add(jti)
+        return {'message': 'Successfully! Logged out'}
+
+
+class TokenRefresh(Resource):
+    @jwt_refresh_token_required
+    def post(self):
+        current_user = get_jwt_identity()
+        new_token = create_access_token(identity=current_user, fresh=False)
+        return {'access_token': new_token}, 200
+
